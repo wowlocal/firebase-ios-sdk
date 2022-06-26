8c8917ec89b78baac215bb5cd440dc3d46b27965
Micci <kh.misha.9@icloud.com>
2022-06-26 17:59:26 +0300

    -symlink- mv

--- Crashlytics/UnitTests/generate_resources.sh
+++ Crashlytics/UnitTests/generate_resources.sh
@@ -15,14 +15,18 @@
 # limitations under the License.
 #
 
-sdk_name=(    "MacOSX" "MacOSX" "AppleTVSimulator" "iPhoneSimulator" )
-target_name=( "macos"  "macos"  "tvos"             "ios" )
-arch=(        "arm64"  "x86_64" "arm64"            "arm64" )
-version=(     "10.15"  "10.15"  "10.0"             "9.0" )
-simulator=(   false    false    true               true )
+echo Building dylib files
+
+sdk_name=(    "MacOSX" "AppleTVSimulator" "iPhoneSimulator" )
+target_name=( "macos"  "tvos"             "ios" )
+version=(     "10.15"  "10.0"             "9.0" )
+simulator=(   false    true               true )
+
+archs=( "arm64" "x86_64" )
 
 function target() {
-	local retVal="${arch[$1]}-apple-${target_name[$1]}${version[$1]}"
+	local arch=$2
+	local retVal="${arch}-apple-${target_name[$1]}${version[$1]}"
 	if [ ${simulator[$1]} == true ]; then
 		retVal="$retVal-simulator"
 	fi
@@ -43,24 +47,28 @@ cd build
 # Generate dylib for each file from dylib_stubs directory
 find .. -iname "*.c" | while read path_to_file
 do
-	for i in {0..3}; do
-		out_dir="${target_name[$i]}_${arch[$i]}"
-		mkdir -p $out_dir && cd $out_dir
-		filename=`basename $path_to_file`
+	for platform in {0..2}; do
+		for arch in ${archs[@]}; do
+			out_dir="${target_name[$platform]}_${arch}"
+			mkdir -p $out_dir && cd $out_dir
+			filename=`basename $path_to_file`
 
-		clang "../$path_to_file" -dynamiclib -o "${filename%.*}".dylib -target $(target $i) -isysroot $(sdk_path $i)
+			clang "../$path_to_file" -dynamiclib \
+				-o "${filename%.*}".dylib \
+				-target $(target $platform $arch) \
+				-isysroot $(sdk_path $platform)
 
-		cd ..
+			cd ..
+		done
 	done
 done
 
-ln -s ios_arm64 ios
-ln -s tvos_arm64 tvos
-
-mkdir macos
-find macos_arm64 -iname "*.dylib" | while read arm64_dylib_path
-do
-	dylib_name=`basename $arm64_dylib_path`
-	x86_64_dylib_path=`find macos_x86_64 -name "$dylib_name"`
-	lipo -create "$arm64_dylib_path" "$x86_64_dylib_path" -output "macos/$dylib_name"
+for target in ${target_name[@]}; do
+	mkdir -p $target
+	find ${target}_arm64 -iname "*.dylib" | while read arm64_dylib_path
+	do
+		dylib_name=`basename $arm64_dylib_path`
+		x86_64_dylib_path=`find ${target}_x86_64 -name "$dylib_name"`
+		lipo -create "$arm64_dylib_path" "$x86_64_dylib_path" -output "${target}/$dylib_name"
+	done
 done
